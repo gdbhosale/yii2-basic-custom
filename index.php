eebee7e43155240c1981627eaa0a2aa6e2e04c71
Ganesh Bhosale <ganesh@dwij.co.in>
2014-10-03 01:09:48 +0530

    shift to root dir

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 
-$config = require(__DIR__ . '/../config/web.php');
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/config/web.php');
 
 (new yii\web\Application($config))->run();
